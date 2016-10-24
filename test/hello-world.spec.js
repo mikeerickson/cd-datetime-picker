2fedc12214cb8bc3dfdfff2c39901c287a6dedb2
Mike Erickson <merickson@cirruspath.com>
2016-10-24 11:15:26 -0700

    fix lint issues

--- test/hello-world.spec.js
+++ test/hello-world.spec.js
@@ -4,8 +4,4 @@ describe('perform mocha test', function () {
   it('should pass', function () {
     expect(true).to.be.true;
   });
-
-  it('should pass', function () {
-    expect(true).to.be.true
-  })
 });
