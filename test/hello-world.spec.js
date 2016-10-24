78928041b7ceab9d76e0c3bb20543ae5081e46ac
Mike Erickson <merickson@cirruspath.com>
2016-10-24 11:14:57 -0700

    testing again, removed sass-lint for now

--- test/hello-world.spec.js
+++ test/hello-world.spec.js
@@ -4,4 +4,8 @@ describe('perform mocha test', function () {
   it('should pass', function () {
     expect(true).to.be.true;
   });
+
+  it('should pass', function () {
+    expect(true).to.be.true
+  })
 });
